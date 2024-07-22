ecdc872bd2fca2f20720653bd74325cab837a97b
Stjoo0925 <stjoo0925@gmail.com>
2024-07-22 22:09:25 +0900

    feat : add project bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <script setup>
-import Header from './components/app/header/header.vue';
-import Footer from './components/app/footer/footer.vue';
-import Section from './components/app/article/section.vue';
-import TopButton from './components/app/top-button.vue';
+import Header from "./components/app/header/header.vue";
+import Footer from "./components/app/footer/footer.vue";
+import Section from "./components/app/article/section.vue";
+import TopButton from "./components/app/top-button.vue";
 </script>
 
 <template>
-    <div class="header">
-        <Header/>
-    </div>
-    <div>
-        <Section/>
-    </div>
+  <div class="header">
+    <Header />
+  </div>
+  <div>
+    <Section />
+  </div>
 
-    <div>
-        <Footer/>
-    </div>
-    <TopButton/>
-
+  <div>
+    <Footer />
+  </div>
+  <TopButton />
 </template>
 
 <style glodal>
@@ -25,8 +24,8 @@ import TopButton from './components/app/top-button.vue';
 @import url("@/assets/font.css");
 
 .header {
-    position: sticky;
-    top: 0;
-    z-index: 1000;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
 }
 </style>
